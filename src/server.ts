5bda1a94c165e3a4f577c73825c01a9438ba606d
Caio Louro <caio.louro@vtex.com.br>
2020-08-10 23:02:53 -0300

    more basic routes to test insomnia

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 app.get('/users', (req, res) => {
+    console.log('query', req.query)
+    return res.json([{ name: 'Yo' }, { name: 'Yo2' }])
+})
+
+app.post('/users', (req, res) => {
+    console.log('body', req.body)
+    return res.json([{ name: 'Yo' }, { name: 'Yo2' }])
+})
+
+app.delete('/users/:id', (req, res) => {
+    console.log('params', req.params)
     return res.json([{ name: 'Yo' }, { name: 'Yo2' }])
 })
 
